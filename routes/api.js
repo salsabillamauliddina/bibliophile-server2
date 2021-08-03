03dea5cb8ee323b29ee6395cdfe392c725cc24d4
Salsabilla Mauliddina <salsabilla.mauliddina@gmail.com>
2021-08-03 10:03:55 +0700

    try to deploy

--- routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const ApiController = require('../controllers/ApiController')
-
-router.get('/popular-book', ApiController.getBookTop)
-router.get('/article-books', ApiController.getArticleBooks)
-// router.get('/quote', ApiController.randomQuote )
-
-module.exports = router;
